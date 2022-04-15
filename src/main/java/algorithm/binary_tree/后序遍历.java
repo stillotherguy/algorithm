77773fea7ac6bb6b2fb51f3b6def95641e550774
Ethan <jizhang03@coupang.com>
2022-04-16 01:44:05 +0800

    binary tree traverse

--- src/main/java/algorithm/binary_tree/后序遍历.java
+++ src/main/java/algorithm/binary_tree/后序遍历.java
@@ -58,4 +58,30 @@ public class 后序遍历 {
             }
         }
     }
+
+    public static void postOrder3(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        s1.push(root);
+        while (!s1.isEmpty()) {
+            root = s1.pop();
+            s2.push(root);
+
+            if (root.left != null) {
+                s2.push(root.left);
+            }
+
+            if (root.right != null) {
+                s2.push(root.right);
+            }
+        }
+
+        while (!s2.isEmpty()) {
+            System.out.println(s2.pop().value);
+        }
+    }
 }

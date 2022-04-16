4416b74088ca18edc6012fc670eed45a001b485d
Ethan <jizhang03@coupang.com>
2022-04-17 01:51:23 +0800

    Morris遍历

--- src/main/java/algorithm/binary_tree/中序遍历.java
+++ src/main/java/algorithm/binary_tree/中序遍历.java
@@ -17,7 +17,7 @@ import java.util.Stack;
 public class 中序遍历 {
 
     public static void main(String[] args) {
-        TreeNode root = BinaryTree.create(Lists.newArrayList(1, 2, 3, 4, 5, 6, 7));
+        TreeNode root = BinaryTree.create(Lists.newArrayList(1, 2, 3, 4, 5, 6));
 
         inOrder1(root);
         inOrder2(root);
@@ -35,22 +35,22 @@ public class 中序遍历 {
         inOrder1(root.right);
     }
 
-    public static void inOrder2(TreeNode root) {
-        if (root == null) {
+    public static void inOrder2(TreeNode head) {
+        if (head == null) {
             return;
         }
 
         Stack<TreeNode> stack = new Stack<>();
-        while (!stack.isEmpty() || root != null) {
-            if (root != null) {
-                stack.push(root);
-                root = root.left;
+        while (!stack.isEmpty() || head != null) {
+            if (head != null) {
+                stack.push(head);
+                head = head.left;
             } else {
-                root = stack.pop();
+                head = stack.pop();
 
-                System.out.println(root.value);
+                System.out.println(head.value);
 
-                stack.push(root.right);
+                head = head.right;
             }
         }
     }

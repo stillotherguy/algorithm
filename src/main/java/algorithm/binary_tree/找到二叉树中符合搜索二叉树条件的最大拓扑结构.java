5b8b095e4bcdb68b6104846d7311fdacbf46c4c7
Ethan Zhang <stillotherguy@gmail.com>
2022-04-20 00:20:10 +0800

    二叉树最大拓扑结构

--- src/main/java/algorithm/binary_tree/找到二叉树中符合搜索二叉树条件的最大拓扑结构.java
+++ src/main/java/algorithm/binary_tree/找到二叉树中符合搜索二叉树条件的最大拓扑结构.java
@@ -8,9 +8,35 @@ import algorithm.utils.TreeNode;
  */
 public class 找到二叉树中符合搜索二叉树条件的最大拓扑结构 {
 
-    public int bstTopSize1(TreeNode head) {
+    public int bstTopoSize1(TreeNode head) {
+        if (head == null) {
+            return 0;
+        }
 
+        int maxTopoSize = maxTopo(head, head);
+        maxTopoSize = Math.max(bstTopoSize1(head.left), maxTopoSize);
+        maxTopoSize = Math.max(bstTopoSize1(head.right), maxTopoSize);
+
+        return maxTopoSize;
+    }
+
+    private int maxTopo(TreeNode h, TreeNode n) {
+        if (h != null && n != null && isBSTNode(h, n, n.value)) {
+            return maxTopo(h, n.left) + maxTopo(h, n.right) + 1;
+        }
 
         return 0;
+    }
+
+    private boolean isBSTNode(TreeNode h, TreeNode n, int value) {
+        if (h == null) {
+            return false;
+        }
+
+        if (h == n) {
+            return true;
+        }
+
+        return isBSTNode(value > h.value ? h.right : h.left, n, value);
     }
 }

5d9d313d25c2049edb854d87df341a5d3dea7c55
Ethan Zhang <stillotherguy@gmail.com>
2022-05-02 22:22:31 +0800

    deserialize binary tree

--- src/main/java/algorithm/binary_tree/二叉树的序列化和反序列化.java
+++ src/main/java/algorithm/binary_tree/二叉树的序列化和反序列化.java
@@ -3,6 +3,8 @@ package algorithm.binary_tree;
 import algorithm.data_structure.BinaryTree;
 import algorithm.utils.TreeNode;
 import com.google.common.collect.Lists;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -11,6 +13,8 @@ import java.util.Stack;
  */
 public class 二叉树的序列化和反序列化 {
 
+    private static final String EMPTY_NODE = "#";
+
     public static void main(String[] args) {
         TreeNode head = BinaryTree.create(Lists.newArrayList(1, 2, 3, 4, 5, 6));
 
@@ -18,7 +22,9 @@ public class 二叉树的序列化和反序列化 {
         System.out.println(preSerialize1(head));
     }
 
+
     public static String preSerialize(TreeNode head) {
+        // 先序遍历序列化
         if (head == null) {
             return "";
         }
@@ -47,6 +53,7 @@ public class 二叉树的序列化和反序列化 {
     }
 
     public static String preSerialize1(TreeNode head) {
+        // 先序遍历序列化
         if (head == null) {
             return "#!";
         }
@@ -56,5 +63,32 @@ public class 二叉树的序列化和反序列化 {
         result += preSerialize1(head.right);
 
         return result;
+    }
+
+    public static TreeNode preDeserialize(String str) {
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+
+        String[] segments = str.split("!");
+        Queue<String> queue = new LinkedList<>();
+        for (String segment : segments) {
+            queue.offer(segment);
+        }
+
+        return preDeserializeRecursive(queue);
+    }
+
+    private static TreeNode preDeserializeRecursive(Queue<String> queue) {
+        String value = queue.poll();
+        if (EMPTY_NODE.equals(value)) {
+            return null;
+        }
+
+        TreeNode head = new TreeNode(Integer.parseInt(value));
+        head.left = preDeserializeRecursive(queue);
+        head.right = preDeserializeRecursive(queue);
+
+        return head;
     }
 }

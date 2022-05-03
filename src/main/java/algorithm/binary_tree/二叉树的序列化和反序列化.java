b40f2ccb9120056eb88481267339bbf9db557327
Ethan Zhang <stillotherguy@gmail.com>
2022-05-03 17:57:50 +0800

    serialize/deserialize binary tree by level

--- src/main/java/algorithm/binary_tree/二叉树的序列化和反序列化.java
+++ src/main/java/algorithm/binary_tree/二叉树的序列化和反序列化.java
@@ -13,7 +13,7 @@ import java.util.Stack;
  */
 public class 二叉树的序列化和反序列化 {
 
-    private static final String EMPTY_NODE = "#";
+    private static final String EMPTY_VALUE = "#";
 
     public static void main(String[] args) {
         TreeNode head = BinaryTree.create(Lists.newArrayList(1, 2, 3, 4, 5, 6));
@@ -81,7 +81,7 @@ public class 二叉树的序列化和反序列化 {
 
     private static TreeNode preDeserializeRecursive(Queue<String> queue) {
         String value = queue.poll();
-        if (EMPTY_NODE.equals(value)) {
+        if (EMPTY_VALUE.equals(value)) {
             return null;
         }
 
@@ -90,5 +90,72 @@ public class 二叉树的序列化和反序列化 {
         head.right = preDeserializeRecursive(queue);
 
         return head;
+    }
+
+    public static String serializeByLevel(TreeNode head) {
+        if (head == null) {
+            return EMPTY_VALUE;
+        }
+
+        String res = head.value + "!";
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(head);
+
+        while (!queue.isEmpty()) {
+            head = queue.poll();
+            if (head.left != null) {
+                res += head.left.value + "!";
+                queue.offer(head.left);
+            } else {
+                res += EMPTY_VALUE;
+            }
+
+            if (head.right != null) {
+                res += head.right.value + "!";
+                queue.offer(head.right);
+            } else {
+                res += EMPTY_VALUE;
+            }
+        }
+
+        return res;
+    }
+
+    public static TreeNode deserializeByLevel(String str) {
+        String[] segments = str.split("!");
+
+        int index = 0;
+
+        TreeNode head = generateNode(segments[index++]);
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (head != null) {
+            queue.offer(head);
+        }
+
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            cur.left = generateNode(segments[index++]);
+            cur.right = generateNode(segments[index++]);
+
+            if (cur.left != null) {
+                queue.offer(cur.left);
+            }
+
+            if (cur.right != null) {
+                queue.offer(cur.right);
+            }
+        }
+
+        return head;
+    }
+
+    private static TreeNode generateNode(String segment) {
+        if (segment.equals("#")) {
+            return null;
+        }
+
+        return new TreeNode(Integer.parseInt(segment));
     }
 }

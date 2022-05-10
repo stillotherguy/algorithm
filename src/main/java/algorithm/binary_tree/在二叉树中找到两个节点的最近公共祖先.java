6e024bc76942acbe82d7dfe5ae18cba42507cbd0
Ethan Zhang <stillotherguy@gmail.com>
2022-05-10 22:23:06 +0800

    find common parent node with data-driven style

--- src/main/java/algorithm/binary_tree/在二叉树中找到两个节点的最近公共祖先.java
+++ src/main/java/algorithm/binary_tree/在二叉树中找到两个节点的最近公共祖先.java
@@ -1,12 +1,19 @@
 package algorithm.binary_tree;
 
 import algorithm.utils.TreeNode;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Ethan Zhang
  * @date 2022/5/9
  */
 public class 在二叉树中找到两个节点的最近公共祖先 {
+
+    public static void main(String[] args) {
+    }
 
     public static TreeNode findParent(TreeNode head, TreeNode o1, TreeNode o2) {
         if (head == null || head == o1 || head == o2) {
@@ -20,5 +27,54 @@ public class 在二叉树中找到两个节点的最近公共祖先 {
         }
 
         return left != null ? left : right;
+    }
+
+    public static TreeNode findParent1(TreeNode head, TreeNode o1, TreeNode o2) {
+        ParentFinder finder = new ParentFinder(head);
+        return finder.find(o1, o2);
+    }
+
+    private static class ParentFinder {
+        private Map<TreeNode, TreeNode> cache;
+
+        public ParentFinder(TreeNode head) {
+            this.cache = new HashMap<>();
+            if (head != null) {
+                this.cache.put(head, null);
+            }
+
+            setMap(head);
+        }
+
+        private void setMap(TreeNode head) {
+            if (head == null) {
+                return;
+            }
+
+            if (head.left != null) {
+                this.cache.put(head.left, head);
+            }
+
+            if (head.right != null) {
+                this.cache.put(head.right, head);
+            }
+
+            setMap(head.left);
+            setMap(head.right);
+        }
+
+        public TreeNode find(TreeNode o1, TreeNode o2) {
+            Set<TreeNode> path = new HashSet<>();
+            while (this.cache.containsKey(o1)) {
+                    path.add(o1);
+                    o1 = this.cache.get(o1);
+            }
+
+            while (!path.contains(o2)) {
+                o2 = this.cache.get(o2);
+            }
+
+            return o2;
+        }
     }
 }

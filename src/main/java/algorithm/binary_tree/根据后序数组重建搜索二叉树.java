aa490ecfd2f53542a8c73d26b8a657dcba63b592
Ethan Zhang <stillotherguy@gmail.com>
2022-05-12 23:41:21 +0800

    restore BST test cases

--- src/main/java/algorithm/binary_tree/根据后序数组重建搜索二叉树.java
+++ src/main/java/algorithm/binary_tree/根据后序数组重建搜索二叉树.java
@@ -1,6 +1,7 @@
 package algorithm.binary_tree;
 
 import algorithm.utils.TreeNode;
+import org.springframework.util.Assert;
 
 /**
  * @author Ethan Zhang
@@ -9,7 +10,17 @@ import algorithm.utils.TreeNode;
 public class 根据后序数组重建搜索二叉树 {
 
     public static void main(String[] args) {
+        int[] post = new int[]{1, 3, 2, 5, 7, 6, 4};
 
+        TreeNode head = restoreByPost(post);
+
+        Assert.isTrue(head.value == 4);
+        Assert.isTrue(head.left.value == 2);
+        Assert.isTrue(head.right.value == 6);
+
+        head = restoreByPost(new int[0]);
+
+        Assert.isTrue(head == null);
     }
 
     public static TreeNode restoreByPost(int[] array) {
@@ -35,8 +46,11 @@ public class 根据后序数组重建搜索二叉树 {
             }
         }
 
-        int mid = (start + end) / 2;
         TreeNode head = new TreeNode(array[end]);
+        if (more == less) {
+            return head;
+        }
+
         head.left = restoreByPostRecursively(array, start, less);
         head.right = restoreByPostRecursively(array, more, end - 1);
 
